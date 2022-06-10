fe688e4321166d1843d8f552244cb998c772fdb4
KSHITIJA <KSHITIJA@DESKTOP-CQO9V82>
2022-06-10 11:16:44 +0530

    commit

---
+++ Java Training/src/training/array.java
@@ -0,0 +1,12 @@
+package training;
+
+public class array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float[] a=new float[3];
+		System.out.println(a.getClass().getName());
+
+	}
+
+}
---
+++ Java Training/src/training/corejava.java
@@ -0,0 +1,11 @@
+package training;
+
+public class corejava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TNS India");
+
+	}
+
+}
